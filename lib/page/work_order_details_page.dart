02157d8dee73b7d449a51c51ed7690d8482aac03
wfreit4s <wfreit4s@tracian.com>
2024-05-12 19:08:11 -0300

    refactor: remove code comment

--- lib/page/work_order_details_page.dart
+++ lib/page/work_order_details_page.dart
@@ -5,7 +5,6 @@ import 'package:work_order/repositories/work_order_repository.dart';
 import 'package:work_order/theme/custom_black_theme.dart';
 
 class WorkOrderDetailsPage extends StatelessWidget {
-  // WorkOrder workOrder;
   final WorkOrder workOrder;
   final _workOrderTitle = TextEditingController();
   final _workOrderDescription = TextEditingController();
@@ -14,25 +13,6 @@ class WorkOrderDetailsPage extends StatelessWidget {
     _workOrderTitle.text = workOrder.title;
     _workOrderDescription.text = workOrder.description;
   }
-
-  // final _form = GlobalKey<FormState>();
-
-  // String description = '';  
-
-  // salvar() {
-  //   if (_form.currentState!.validate()) {
-  //     // Salvar a compra
-
-  //     Navigator.pop(context);
-
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       const SnackBar(
-  //         content: Text('Work Order saved!'),
-  //         duration: Duration(seconds: 2),
-  //       ),
-  //     );
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -70,7 +50,15 @@ class WorkOrderDetailsPage extends StatelessWidget {
                         }
                         Navigator.of(context).pop();
                       },
-                      icon: Icon(!workOrder.deleted ? Icons.delete : Icons.restore),
+                      icon: !workOrder.deleted
+                        ? const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.delete, size: 20),
+                              Icon(Icons.close, size: 20),
+                            ],
+                          )
+                        : const Icon(Icons.restore),
                     ),
                   ],
                 ),
@@ -144,78 +132,5 @@ class WorkOrderDetailsPage extends StatelessWidget {
         ),
       ),
     );
-
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     title: Text('${widget.workOrder.title} (#${widget.workOrder.number})'),
-    //   ),
-    //   body: Padding(
-    //     padding: const EdgeInsets.all(24),
-    //     child: Column(
-    //       children: [
-    //         // const Padding(
-    //         //   padding: EdgeInsets.only(bottom: 5),
-    //         //   child: Row(
-    //         //     children: [
-    //         //       Column(
-    //         //         children: [
-    //         //           Text('Description:', style: TextStyle(
-    //         //             fontSize: 22,
-    //         //             fontWeight: FontWeight.w500,
-    //         //           )),                  
-    //         //         ],
-    //         //       ),                  
-    //         //     ],
-    //         //   ),
-    //         // ),            
-    //         Form(
-    //           key: _form,
-    //           child: TextFormField(
-    //             controller: TextEditingController(text: widget.workOrder.description),
-    //             style: const TextStyle(
-    //               fontSize: 20,
-    //               fontWeight: FontWeight.w400,
-    //             ),
-    //             decoration: const InputDecoration(
-    //               border: OutlineInputBorder(),
-    //               labelText: 'Description',              
-    //             ),
-    //             keyboardType: TextInputType.text,
-    //             validator: (value) {
-    //               if (value!.isEmpty) {
-    //                 return 'Please enter a description';
-    //               } else if (value.length < 2) {
-    //                 return 'Please enter a longer description';
-    //               }
-    //               return null;
-    //             },
-    //             onChanged: (value) {
-    //               setState(() {
-    //                 widget.workOrder.description = value;
-    //               });
-    //             }
-    //           ),
-    //         ),
-    //         Container(
-    //           alignment: Alignment.bottomCenter,
-    //           margin: const EdgeInsets.only(top: 24),
-    //           child: ElevatedButton(
-    //             onPressed: salvar,
-    //             child: const Row(
-    //               mainAxisAlignment: MainAxisAlignment.center,
-    //               children: [
-    //                 Icon(Icons.save),
-    //                 Padding(
-    //                   padding: EdgeInsets.all(10),
-    //                   child: Text('Save', style: TextStyle(fontSize: 20)),
-    //                 ),
-    //               ],
-    //             ),
-    //           ),
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
   }
 }
